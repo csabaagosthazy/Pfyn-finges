fc4c3e734a25eccf457b24461408eee5d3ab2407
csabaagosthazy <agosthazy.csaba@gmail.com>
2021-10-11 12:36:10 +0200

    bug fixes

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,39 +1,43 @@
-import React, {Component} from 'react';
-import {MapContainer, Polyline, TileLayer} from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import {getPois} from "../services/dbService";
+import React, { Component } from "react";
+import { MapContainer, Polyline, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { getPois } from "../services/dbService";
 
 class MapView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentLocation: {lat: 46.294574, lng: 7.569767},
-            zoom: 16,
-            positions : " ",
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: { lat: 46.294574, lng: 7.569767 },
+      zoom: 16,
+      positions: "",
+    };
+  }
 
-    async componentDidMount() {
-        const positions = await getPois();
-        this.setState({positions});
-    }
+  async componentDidMount() {
+    const positions = await getPois();
+    console.log(positions);
+    this.setState({ positions });
+  }
 
-    render() {
-        const {currentLocation, zoom} = this.state;
-
-        return (
-            <MapContainer center={currentLocation} zoom={zoom} style={{height: '720px', width: '1280px'}}>
-                <TileLayer
-                    url="https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg"
-                />
-                <Polyline
-                    pathOptions={{fillColor: 'red', color: 'orange', weight: 6}}
-                    positions={this.state.positions}
-                />
-                {/* <Markers venues={data.venues}/> */}
-            </MapContainer>
-        );
-    }
+  render() {
+    const { currentLocation, zoom } = this.state;
+    console.log(this.state.positions);
+    if (this.state.positions === "") return <p>Loading</p>;
+    return (
+      <MapContainer
+        center={currentLocation}
+        zoom={zoom}
+        style={{ height: "720px", width: "1280px" }}
+      >
+        <TileLayer url="https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg" />
+        <Polyline
+          pathOptions={{ fillColor: "red", color: "orange", weight: 6 }}
+          positions={this.state.positions}
+        />
+        {/* <Markers venues={data.venues}/> */}
+      </MapContainer>
+    );
+  }
 }
 
 export default MapView;
