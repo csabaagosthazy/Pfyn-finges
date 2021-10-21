6cacde4e5c779cc446a535c36cfb121c6e9bbea3
csabaagosthazy <agosthazy.csaba@gmail.com>
2021-10-21 16:31:15 +0200

    bug fix

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,54 +1,52 @@
-import {useAuth} from "../context/AuthContext";
-import {getGPXAsString, getPoisByUser, getUserParams} from "../services/dbService";
+import { useAuth } from "../context/AuthContext";
+import { getGPXAsString, getPoisByUser, getUserParams } from "../services/dbService";
 import MapView from "../components/MapView";
-import {useEffect, useState} from "react";
-import {Dropdown} from "react-bootstrap";
-
+import { useEffect, useState } from "react";
+import { Dropdown } from "react-bootstrap";
 
 const UserPage = () => {
-    const {user, signOut} = useAuth();
-    const [gpxHistory, setgpxHistory] = useState();
-    const [pois, setPois] = useState([]);
+  const { user, signOut } = useAuth();
+  const [gpxHistory, setgpxHistory] = useState();
+  const [pois, setPois] = useState([]);
 
-    useEffect(async () => {
-        let history = await getGPXAsString(user);
-        setgpxHistory(history);
-        console.log(getUserParams((user)));
+  useEffect(async () => {
+    let history = await getGPXAsString(user);
+    setgpxHistory(history);
+    console.log(getUserParams(user));
 
-        console.log(user);
-        let poisList = await getPoisByUser(user)
-        setPois(poisList);
-    }, [])
+    console.log(user);
+    let poisList = await getPoisByUser(user);
+    setPois(poisList);
+  }, []);
 
+  function handleClick(event) {
+    event.preventDefault();
+    console.log(event.target.name);
+  }
 
+  if (!gpxHistory || !pois) return <p>"Loading"</p>;
 
+  return (
+    <>
+      <h1>Welcome on user page {user?.email}</h1>
+      <button onClick={() => signOut()}>Sign out</button>
+      <MapView pois={pois} />
 
-    function handleClick(event) {
-        event.preventDefault();
-        console.log(event.target.name);
-    }
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          History
+        </Dropdown.Toggle>
 
-    if (!gpxHistory) return <p>"Loading"</p>
-
-    return (
-        <>
-            <h1>Welcome on user page {user?.email}</h1>
-            <button onClick={() => signOut()}>Sign out</button>
-            <MapView pois = {pois}/>
-
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    History
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    {gpxHistory.map((jesus, index) =>
-                        <Dropdown.Item name={jesus} onClick={(event) => handleClick(event)} key={index}>{jesus}</Dropdown.Item>
-                    )}
-                </Dropdown.Menu>
-            </Dropdown>
-        </>
-    );
+        <Dropdown.Menu>
+          {gpxHistory.map((jesus, index) => (
+            <Dropdown.Item name={jesus} onClick={(event) => handleClick(event)} key={index}>
+              {jesus}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
 };
 
 export default UserPage;
