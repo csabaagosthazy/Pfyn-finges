b4aaaab02c9c8771e27cb1bc8a112f681c0fd15b
paulo.aguiar <aguiarpaulo98@gmail.com>
2021-11-04 10:14:50 +0100

    Correcting language changement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useAuth } from "../context/Auth2";
 import { useHistory } from "react-router-dom";
 import forest from "../img/2040.jpg";
+import {useLang, languages} from "../context/LanguageContext";
 
 const HomePage = () => {
   const { currentUser, isAdmin } = useAuth();
+  const {language} = useLang();
   let history = useHistory();
 
   React.useEffect(() => {
@@ -19,15 +21,15 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Welcome to the Pfinges forest website</h1>
-      <img src={forest} alt="Forest image" width="800" />
-      <p>
-        <a href="https://fr.freepik.com/vecteurs/fond">
-          Fond vecteur créé par upklyak - fr.freepik.com
-        </a>
-      </p>
-    </div>
+      <div>
+        <h1>{languages[language].welcome_title}</h1>
+        <img src={forest} alt="Forest image" width="800" />
+        <p>
+          <a href="https://fr.freepik.com/vecteurs/fond">
+            Fond vecteur créé par upklyak - fr.freepik.com
+          </a>
+        </p>
+      </div>
   );
 };
 
